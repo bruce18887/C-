c7cf3077411f9a81acc0591d0ba0fac0ecec24c4
bruce18887 <1597865041@qq.com>
2024-03-05 15:06:33 +0800

    some func will rebuild for msvc

---
+++ FuncForMSVC/L2/L2.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+using namespace std;
+
+
+string DecToHex(int decimal, int fixlength = 0, bool prefix = false) {
+    string hex = "";
+    if (decimal == 0)hex = "0";
+    else {
+        while (decimal > 0) {
+            int remainder = decimal % 16;
+            char hexDigit = (remainder < 10) ? (remainder + '0') : (remainder - 10 + 'A');
+            hex += hexDigit;
+            decimal /= 16;
+        }
+        reverse(hex.begin(), hex.end());
+    }
+
+    if (fixlength > hex.length())hex = string(fixlength - hex.length(), '0') + hex;
+    if (prefix) hex = "0x" + hex;
+   
+
+    return hex;
+}
+
+
+int main()
+{
+    cout << DecToHex(0x0,4,false) << endl;
+    system("pause");
+    return 0;
+}
