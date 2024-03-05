905eee2fe956a6f1291500a5d252d33c7459dec3
bruce18887 <1597865041@qq.com>
2024-03-05 16:23:36 +0800

    update

--- FuncForMSVC/L2/L2.cpp
+++ FuncForMSVC/L2/L2.cpp
@@ -1,5 +1,7 @@
 ﻿#include <iostream>
-
+#include <Windows.h>
+#include <string>
+#include <sstream>
 using namespace std;
 
 
@@ -15,18 +17,42 @@ string DecToHex(int decimal, int fixlength = 0, bool prefix = false) {
         }
         reverse(hex.begin(), hex.end());
     }
-
     if (fixlength > hex.length())hex = string(fixlength - hex.length(), '0') + hex;
     if (prefix) hex = "0x" + hex;
-   
-
     return hex;
 }
+
+//C 语言 风格
+void DebugPrint(const char* format, ...) {
+    char buffer[1024];
+    va_list args;
+    va_start(args, format);
+    vsnprintf(buffer, sizeof(buffer), format, args);
+    va_end(args);
+    OutputDebugStringA(buffer);
+}
+// C++ cout 风格 不支持 endl
+class DebugPrintC {
+public:
+    template <typename T>
+    DebugPrintC& operator<<(const T& value) {
+        std::ostringstream oss;
+        oss << value;
+        OutputDebugStringA(oss.str().c_str());
+        return *this;
+    }
+};
+
+DebugPrintC dcout;
+
 
 
 int main()
 {
     cout << DecToHex(0x0,4,false) << endl;
+    DebugPrint("Site:%d test result is %f %s \n", 1, 3.145, "OK");
+    
+    dcout << "Hello, " << "World! " << 42 << "value is " << 3.1255656 << "\n";
     system("pause");
     return 0;
 }
